93e706e0d2c1810ab53b87d29d80c2af85720434
Akshay Kochhar <aks.kochhar@gmail.com>
2020-12-05 18:19:52 -0500

    created student class

---
+++ Project_Van Rooyen_Kochhar/app/src/main/java/com/example/project_vanrooyen_kochhar/Student.kt
@@ -0,0 +1,11 @@
+package com.example.project_vanrooyen_kochhar
+
+class Student ( var id : Int, var score : Int, var comment : String){
+
+    fun getName(){}
+
+    fun getId(){}
+
+    fun getComment(){}
+
+}
