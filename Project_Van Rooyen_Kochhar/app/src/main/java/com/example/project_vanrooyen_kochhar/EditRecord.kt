2038e2484bbd9c0536eb38987df319b2ed8ae85b
Akshay Kochhar <aks.kochhar@gmail.com>
2020-12-05 17:43:05 -0500

    all activities set up

---
+++ Project_Van Rooyen_Kochhar/app/src/main/java/com/example/project_vanrooyen_kochhar/EditRecord.kt
@@ -0,0 +1,11 @@
+package com.example.project_vanrooyen_kochhar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditRecord : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_record)
+    }
+}
