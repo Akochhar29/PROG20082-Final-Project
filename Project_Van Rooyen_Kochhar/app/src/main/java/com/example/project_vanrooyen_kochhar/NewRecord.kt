ba070b5e5ac7bd456fd4541fe3aa3979708ff3df
Akshay Kochhar <aks.kochhar@gmail.com>
2020-12-05 17:20:43 -0500

    added newrecord

---
+++ Project_Van Rooyen_Kochhar/app/src/main/java/com/example/project_vanrooyen_kochhar/NewRecord.kt
@@ -0,0 +1,11 @@
+package com.example.project_vanrooyen_kochhar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewRecord : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_record)
+    }
+}
