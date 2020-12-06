551ff40bc11e8310a035f336d4485f788b9f8949
Luke Van Rooyen <lukevanrooyen@hotmail.com>
2020-12-05 19:12:01 -0500

    Main Page
    Implemented more of the functions related to the main page

--- Project_Van Rooyen_Kochhar/app/src/main/java/com/example/project_vanrooyen_kochhar/MainScreen.kt
+++ Project_Van Rooyen_Kochhar/app/src/main/java/com/example/project_vanrooyen_kochhar/MainScreen.kt
@@ -1,11 +1,38 @@
 package com.example.project_vanrooyen_kochhar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_login_screen.*
+import kotlinx.android.synthetic.main.activity_main_screen.*
 
 class MainScreen : AppCompatActivity() {
+
+    var choice = Option.NONE
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_screen)
+        selectOption_tv.text = getString(R.string.greet) + intent.getStringExtra("username") + "\n" + getString(R.string.option)
     }
+
+    fun onSubmit(view: View) {
+        if (choice != Option.NONE) {
+            var intentV2 = Intent(this, null)
+
+            when (choice) {
+                Option.NEWRECORD -> intentV2 = Intent(this, NewRecord::class.java)
+                Option.PREVRECORD -> intentV2 = Intent(this, AllRecords::class.java)
+                Option.LOGOUT -> finish() //Logout
+            }
+            startActivity(intent)
+        }
+    }
+}
+
+enum class Option {
+    NONE,
+    NEWRECORD,
+    PREVRECORD,
+    LOGOUT
 }
