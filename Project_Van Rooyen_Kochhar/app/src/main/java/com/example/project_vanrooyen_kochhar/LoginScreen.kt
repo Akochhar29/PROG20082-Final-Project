2d6a2c526e7367f737c9b92b797e0388ab6059f1
Akshay Kochhar <aks.kochhar@gmail.com>
2020-12-10 17:12:14 -0500

    updated loginscreen

--- Project_Van Rooyen_Kochhar/app/src/main/java/com/example/project_vanrooyen_kochhar/LoginScreen.kt
+++ Project_Van Rooyen_Kochhar/app/src/main/java/com/example/project_vanrooyen_kochhar/LoginScreen.kt
@@ -9,9 +9,12 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login_screen.*
 import org.json.JSONObject
 import org.xml.sax.Parser
+import java.io.BufferedReader
+import java.io.InputStream
+import java.lang.Exception
 import java.net.URL
+import javax.net.ssl.HttpsURLConnection
 
-        val loginResponse = "valid"
 class LoginScreen : AppCompatActivity() {
     var username = ""
     var password = ""
@@ -23,48 +26,70 @@ class LoginScreen : AppCompatActivity() {
         names_tv.text = getString(R.string.Akshay) + "\n" + getString(R.string.Luke)
 
         val sharedPrefs = getSharedPreferences(sharePreferences, Context.MODE_PRIVATE)
-        val autoLog = sharedPrefs.getString(username,"")
-        if (autoLog == ""){
-
-        } else {
-            val intent = Intent(this, MainScreen::class.java)
-            intent.putExtra("username", username_et.text.toString())
-            startActivity(intent)
+        val autoLog = sharedPrefs.getString(username, "")
+        if (autoLog != null) {
+            if (autoLog.isNotBlank()) {
+                val intent = Intent(this, MainScreen::class.java)
+                intent.putExtra("username", autoLog)
+                startActivity(intent)
+            }
         }
+
     }
 
     fun login(view: View) {
+        var loginResponse  = ""
         username = username_et.text.toString();
         password = pwd_et.text.toString()
 
-        //val urlResponse = JSONObject(URL("http://mohameom.dev.fast.sheridanc.on.ca/login/verify.php?name=" + username + "&password=" + password).readText())
-        //val loginResponse = urlResponse.getJSONObject("login").toString()
+        var url = URL("http://mohameom.dev.fast.sheridanc.on.ca/login/verify.php?name=" + username + "&password=" + password)
+        loginResponse = loadData(url)
 
-         val loginResponse = "valid"
 
-      if (loginResponse == "valid"){
+        if (loginResponse == "valid") {
 
-           val intent = Intent(this, MainScreen::class.java)
-           intent.putExtra("username", username_et.text.toString())
-           startActivity(intent)
+            val intent = Intent(this, MainScreen::class.java)
+            intent.putExtra("username", username_et.text.toString())
+            startActivity(intent)
 
-           if(keeplogged_cb.isChecked) {
-               val sharedPrefs = getSharedPreferences(sharePreferences, Context.MODE_PRIVATE)
-               val editor = sharedPrefs.edit()
+            if (keeplogged_cb.isChecked) {
+                val sharedPrefs = getSharedPreferences(sharePreferences, Context.MODE_PRIVATE)
+                val editor = sharedPrefs.edit()
 
-               editor.putString(username, password);
-               editor.commit()
+                editor.putString(username, password);
+                editor.commit()
+            }
 
-           }
+        } else if (loginResponse == "invalid") {
+            Toast.makeText(
+                applicationContext,
+                "Wrong password. Please try again.",
+                Toast.LENGTH_SHORT
+            ).show()
 
-       } else if(loginResponse == "invalid"){
-           Toast.makeText(
-               applicationContext,
-               "Wrong password. Please try again.",
-               Toast.LENGTH_SHORT
-           ).show()
+        }
+    }
 
-       }
+    fun loadData(url : URL): String {
+        var ins : InputStream? = null
+        var res = ""
+        var valid = ""
+        try {
+            val conn = url.openConnection() as HttpsURLConnection
+            conn.requestMethod = "GET"
+
+            ins = conn.inputStream
+
+            res = ins.bufferedReader().use(BufferedReader::readText)
+            var json = JSONObject(res)
+            valid = json.optString("login")
+            
+        } catch (e : Exception){
+
+
+        }
+
+        return valid
 
     }
 }
