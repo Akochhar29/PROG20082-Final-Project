d094940e247bf7c52f1f5f7bc4433c0bd2371327
Akshay Kochhar <aks.kochhar@gmail.com>
2020-12-10 17:38:56 -0500

    cath exc login

--- Project_Van Rooyen_Kochhar/app/src/main/java/com/example/project_vanrooyen_kochhar/LoginScreen.kt
+++ Project_Van Rooyen_Kochhar/app/src/main/java/com/example/project_vanrooyen_kochhar/LoginScreen.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login_screen.*
@@ -87,6 +88,7 @@ class LoginScreen : AppCompatActivity() {
             
         } catch (e : Exception){
 
+            Log.d("TAG", e.toString())
 
         }
 
